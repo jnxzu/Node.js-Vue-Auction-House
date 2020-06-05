23212da1ed00ed5fab678a5ccb01e6b86e0600bd
J <janbielowka@gmail.com>
2020-06-05 09:15:01 +0200

    https on heroku

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const moment = require("moment");
 
 const app = express();
-const server = require("./config/https")(app);
+// const server = require("./config/https")(app);
 
 const io = require("socket.io")(server);
 require("./config/socket.io")(io);
@@ -34,6 +34,12 @@ app.use("/js", express.static(path.join(__dirname, "public", "js")));
 app.use("/css", express.static(path.join(__dirname, "public", "css")));
 app.use("/img", express.static(path.join(__dirname, "public", "img")));
 
-server.listen(() => {
+const port = process.env.PORT || 3000;
+
+// server.listen(port,() => {
+//   console.log(`${moment().format("MMMM Do YYYY, h:mm:ss a")} - Server live. https://localhost:${port}`);
+// });
+
+app.listen(port, () => {
   console.log(`${moment().format("MMMM Do YYYY, h:mm:ss a")} - Server live.`);
 });
